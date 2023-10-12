7fb6b8a0f3bdb5f5380ae450266644dec64269a1
AdamCsiki <112400969+AdamCsiki@users.noreply.github.com>
2023-10-12 18:04:06 +0300

    Update api.ts

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,2 +1,2 @@
 export const basePath =
-	"http://laptop-legion-adam.cerberus-saurolophus.ts.net:8080";
+	"https://cors-anywhere.herokuapp.com/http://laptop-legion-adam.cerberus-saurolophus.ts.net:8080";
